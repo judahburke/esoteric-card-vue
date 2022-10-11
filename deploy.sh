e516eb8a9d6679ce55bff398d291cd7e1387ca5b
Judah Burke <judah.burke@quantumexecutive.net>
2022-10-10 23:42:43 -0500

    202210102342, bug fix 404 error by creatint 404.html on deploy

--- deploy.sh
+++ deploy.sh
@@ -9,21 +9,17 @@ set -e
 npm run build
 
 # navigate into the build output directory
-
-# if you are deploying to a custom domain
-
-# echo 'www.judah.brk' -> CNAME
-
-commitdatetime=
-
 cd dist
 
+# if you are deploying to a custom domain
+# echo 'www.judah.brk' -> CNAME
+
+# copy index.html to 404.html
+cp index.html 404.html
+
 git init
-
 git add -A
-
 git commit -m "`date -u +%Y%m%d%H%M%S`, deploy"
-
 git push -f git@github.com:judahburke/esoteric-card-vue.git master:gh-pages
 
 cd -
